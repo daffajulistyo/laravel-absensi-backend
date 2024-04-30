3616a77a8fe0a94864bfc388ec622185e2a69435
daffajulistyo <daffajulistyo88@gmail.com>
2024-04-30 22:01:59 +0700

    Attendance API

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -23,6 +23,12 @@
                     <span>Company</span>
                 </a>
             </li>
+            <li class="nav-item">
+                <a href="{{ route('attendances.index') }}" class="nav-link">
+                    <i class="fas fa-columns"></i>
+                    <span>Attendances</span>
+                </a>
+            </li>
 
     </aside>
 </div>
